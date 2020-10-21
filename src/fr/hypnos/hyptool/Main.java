9deb096e2dffbd8f5f84b08a8371798c6661e266
Charles <charlesInfo77@users.noreply.github.com>
2020-10-21 18:25:09 +0200

    New commands

--- src/fr/hypnos/hyptool/Main.java
+++ src/fr/hypnos/hyptool/Main.java
@@ -1,22 +1,28 @@
 package fr.hypnos.hyptool;
 
 
+import fr.hypnos.hyptool.commands.Commands;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
-
 
 public class Main extends JavaPlugin {
 
+    public Main plugin;
+
+    public void setPlugin(Main plugin) {
+        this.plugin = plugin;
+    }
+
     @Override
     public void onEnable() {
-
-        Helirium helirium = new Helirium();
-
-        getLogger().info("HypTool successfully loaded !");
-        this.getCommand("helirium").setExecutor(helirium);
+        saveDefaultConfig();
+        getCommand("ht").setExecutor(new Commands(this));
+        getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "HypTool successfully loaded !");
     }
 
     @Override
     public void onDisable() {
         getLogger().info("HypTool disabled.");
     }
+
 }
